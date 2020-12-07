5196a333c5369ac4724aa2912093e7cedca0f387
Jeremy <jeremy.stegall77@gmail.com>
2020-12-07 13:25:16 -0500

    Modified css, added project to Second.js

--- src/components/Second.js
+++ src/components/Second.js
@@ -7,7 +7,7 @@ import javascript from '../Images/javascript.jpeg';
 import node from '../Images/node.jpeg';
 import react from '../Images/react.jpeg';
 import post from '../Images/post.jpeg';
-import sqlz from '../Images/sqlz.jpeg';
+// import sqlz from '../Images/sqlz.jpeg';
 
 function Second() {
     return (
@@ -36,21 +36,38 @@ function Second() {
 
                     <div className="col-sm-12 col-lg-6 seeit">
                         <h3>Projects</h3>
+                        <div className="row">
+                            <div className="col-12 proj">
                         <p>Find recording, rehearsal, production and dance studios to fit your needs and budget with:&nbsp;
                         <a href="http://mystudiosearch.com" target="_blank" rel="noreferrer">Sessions</a></p>
+                        <p><a href="https://github.com/JRStegall/Studio_Search">github (front end repository)</a></p>
+                        <p><a href="https://github.com/JRStegall/Studio_Search_backend">github (back end repository</a></p>
+                        </div>
                         <br/>
+                        <div className="col-12 proj">
+                        <p>React Admin Page Prototype: <a href="https://eloquent-nobel-d2ebde.netlify.app/#/" target="_blank" rel="noreferrer">Admin Page</a></p>
+                        <p><a href="https://github.com/JRStegall/ReactAdmin">github Repository</a></p>
+                        </div>
+                        <br/>
+                        <div className="col-12 proj">
                         <p>Search for any movie you can think of on: &nbsp;
                         <a href="https://gifted-kepler-e3c935.netlify.app" target="_blank" rel="noreferrer">Movie Search</a></p>
+                        <p><a href="https://github.com/JRStegall/React-Movie-Search">github Repository</a></p>
+                        </div>
                         <br/>
+                        <div className="col-12 proj">
                         <p>Keep up with the latest trends in cryptocurrency with:&nbsp;
                         <a href="https://cyptoappp.netlify.app/" target="_blank" rel="noreferrer">CryptoApp</a></p>
+                        <p><a href="https://github.com/JRStegall/cryptoAppp">github Repository</a></p>
+                        </div>
+                        </div>
                     </div>
                         
                     <div className="col-sm-12 col-lg-6 seeit">
 
 
                         <h3>Coding Education</h3>
-
+                        
                         <ul>
                             <li><h4>DigitalCrafts- Atlanta, GA</h4></li>
                             <p>DigitalCrafts is Atlanta's premiere software development bootcamp and has been educating coders since 2015. </p>
