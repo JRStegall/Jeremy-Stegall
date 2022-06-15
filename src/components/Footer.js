3d94f00b5ecfcaf772b19bf129af4333cc063472
Jeremy <jeremy@Jeremys-MacBook-Pro-2.local>
2022-06-15 11:12:36 -0400

    Resume Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from "react";
-import pdf from "../resume/resume.pdf";
+import pdf from "./resume/Jeremy.Stegall.Resume.pdf";
 
 function Footer() {
   return (
     <div className="button">
-      <a href={pdf} download="./public/resume/resume.pdf">
+      <a href={pdf} download="./resume/Jeremy.Stegall.Resume.pdf">
         <button className="btn btn-primary">
           <i className="fa fa-download"></i> Download Resume
         </button>
